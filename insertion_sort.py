58e9b2001e6f2fd020c8332959bcd37fbb5b8bca
vemulaprasanna <prasannavemula937@gmail.com>
2026-08-08 17:26:40 +0530

    Add files via upload

---
+++ insertion_sort.py
@@ -0,0 +1,11 @@
+# 3.Insertion sort
+
+arr= list(map(int,input("Enter elements: ").split()))
+for i in range(1, len(arr)):
+    key=arr[i]
+    j=i-1
+    while j>=0 and arr[j]>key:
+        arr[j+1]=arr[j]
+        j-=1
+    arr[j+1]=key
+print(*arr)
